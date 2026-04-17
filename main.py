e7aa8a8ba9ff0022bc7b902fad1465e853c98339
Snehashis-KOX <snehashisnayek@gmail.com>
2026-04-17 20:00:36 +0530

    Enhance broken link checker with Wayback Machine support
    Added functionality to check for archived snapshots of broken links using Wayback Machine.

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-'''from crawler.spider import extract_links
-
-# url = "https://example.com"
-url = "https://books.toscrape.com/"
-
-links = extract_links(url)
-
-for link in links[:5]:
-    print(link)
-
-print(f"Total links found: {len(links)}")'''
 
 from crawler.spider import extract_links
 from crawler.checker import check_link
+from wayback.archive_api import get_wayback_snapshot, fetch_archived_text
+
 
 url = "https://www.eddymens.com/blog/page-with-broken-pages-for-testing-53049e870421"
 
@@ -19,7 +10,7 @@ links = extract_links(url)
 
 broken_links = []
 
-for link in links[:10]:  # limit for testing
+for link in links[:20]:  # limit for testing
     result = check_link(link)
     if result:
         broken_links.append(result)
@@ -28,3 +19,16 @@ print(f"Broken links found: {len(broken_links)}")
 
 for b in broken_links:
     print(b)
+
+for b in broken_links:
+    print("\nChecking:", b["broken_url"])
+
+    snapshot = get_wayback_snapshot(b["broken_url"])
+
+    if snapshot:#this will get snapshot url from wayback machine
+        print("✅ Snapshot found:", snapshot)
+
+        text = fetch_archived_text(snapshot)#this will fetch and clean text from archived page
+        print("📄 Sample text:", text[:200])
+    else:
+        print("❌ No snapshot found")
